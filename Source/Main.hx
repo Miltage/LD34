2a0376bdbf5e1918b2e4db4bfdc2ab0a62e65e40
Robert Shenton <theshenton@gmail.com>
2022-03-02 08:13:07 +0200

    Add start button

--- Source/Main.hx
+++ Source/Main.hx
@@ -2,10 +2,14 @@ package;
 
 import openfl.display.Sprite;
 import openfl.Lib;
+import openfl.display.BitmapData;
+import openfl.Assets;
+import openfl.events.MouseEvent;
 
 import me.miltage.ld34.Game;
 import me.miltage.ld34.SoundManager;
 import me.miltage.ld34.Menu;
+import me.miltage.ld34.Button;
 
 class Main extends Sprite {
 
@@ -19,22 +23,35 @@ class Main extends Sprite {
 	var soundManager:SoundManager;
 	var menu:Menu;
 	
-	public function new () {
-		
-		super ();
+	public function new () {		
+		super();
 
 		instance = this;
 		Lib.current.stage.quality = flash.display.StageQuality.LOW;
 
+		menu = new Menu();
+		addChild(menu);
+		hideMenu();
+
+		graphics.beginFill(0, 1);
+		graphics.drawRect(0, 0, width, height);
+
+		var menuText:BitmapData = Assets.getBitmapData("assets/start_button_text.png");
+
+		var restartButton:Button = new Button(menuText, 0, 0, function(){});
+		restartButton.x = 400 - 200;
+		restartButton.y = 300 - 32;
+		addChild(restartButton);
+
+		addEventListener(MouseEvent.MOUSE_UP, function(m) { start(); });
+	}
+
+	public function start() {
 		soundManager = new SoundManager(1);
 		soundManager.loop("assets/song.mp3");
 		
 		game = new Game(stage);
 		addChild(game);
-
-		menu = new Menu();
-		addChild(menu);
-		hideMenu();
 	}
 
 	public function restart(){
